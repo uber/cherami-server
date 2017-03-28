0e783f89bbb085d46fe9ca526a33f97d751bfc78
Venkatraghavan Srinivasan <venkat@uber.com>
2017-03-28 10:18:31 -0700

    go fmt

--- common/dconfig/configmgr.go
+++ common/dconfig/configmgr.go
@@ -351,8 +351,8 @@ func (cfgMgr *CassandraConfigManager) mkKVTreeForSvc(service string, items []*m.
 
 	allSkus := make(map[string]struct{})
 	allVersions := make(map[string]struct{})
-	wildcardVersionItems := make([]*m.ServiceConfigItem, 0, len(items)) // items with version as wildcard
-	wildcardVersionSkuItems := make([]*m.ServiceConfigItem, 0, len(items)) // items with version as wildcard and sku not a wildcard
+	wildcardVersionItems := make([]*m.ServiceConfigItem, 0, len(items))              // items with version as wildcard
+	wildcardVersionSkuItems := make([]*m.ServiceConfigItem, 0, len(items))           // items with version as wildcard and sku not a wildcard
 	versionToWildcardSkuItems := make(map[string][]*m.ServiceConfigItem, len(items)) // items with sku as wildcard
 
 	for i, item := range items {
@@ -370,7 +370,7 @@ func (cfgMgr *CassandraConfigManager) mkKVTreeForSvc(service string, items []*m.
 		}
 
 		if sku != wildcardToken && host != wildcardToken {
-			key := strings.Join([]string{service,version, sku, host,cfgKey}, ".")
+			key := strings.Join([]string{service, version, sku, host, cfgKey}, ".")
 			sku = wildcardToken
 			items[i].Sku = common.StringPtr(wildcardToken)
 			cfgMgr.logger.WithField(key, cfgValue).Warn("Forcing sku=*;host specific items MUST be sku agnostic")
@@ -427,7 +427,7 @@ func (cfgMgr *CassandraConfigManager) mkKVTreeForSvc(service string, items []*m.
 
 	// apply svc.*. items as default for every svc.ver.*
 	for _, item := range wildcardVersionItems {
- 		for ver := range allVersions {
+		for ver := range allVersions {
 			for sku := range allSkus {
 				addToKVTree(root, ver, sku, item.GetHostname(), item.GetConfigKey(), item.GetConfigValue())
 			}
@@ -436,7 +436,6 @@ func (cfgMgr *CassandraConfigManager) mkKVTreeForSvc(service string, items []*m.
 
 	return root
 }
-
 
 // mkConfig constructs a config object of given type using the
 // values from the given set of key,value strings. If the given

0396b067fe65f174d99f320b5478f01dcc41c2c8
Venkatraghavan Srinivasan <venkat@uber.com>
2017-04-17 13:42:56 -0700

    change impl of isUP

--- services/controllerhost/dfdd.go
+++ services/controllerhost/dfdd.go
@@ -365,7 +365,8 @@ func (dfdd *dfddImpl) putHosts(service string, hosts map[string]dfddHost) {
 
 func isDfddHostStatusUP(dfdd Dfdd, service string, hostID string) (dfddHostState, bool) {
 	state, _ := dfdd.GetHostState(service, hostID)
-	ok := (state != dfddHostStateDown && state != dfddHostStateUnknown)
+	// GoingDown means host is still UP, but its about to go down shortly
+	ok := (state == dfddHostStateUP || state == dfddHostStateGoingDown)
 	return state, ok
 }
 

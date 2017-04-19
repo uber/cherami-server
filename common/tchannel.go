b021fd60afbcd6a79334e3bbc8a8d97ba6adfec3
Bo Yang <boy@uber.com>
2017-04-19 13:55:19 -0700

    Fix lint issue

--- common/tchannel.go
+++ common/tchannel.go
@@ -6,32 +6,37 @@ import (
 )
 
 type (
+	// TChanAuthorizerFilter intercepts each tchannal call and applies authorization
 	TChanAuthorizerFilter struct {
 		ChanServer thrift.TChanServer
 		Authorizer Authorizer
 	}
 )
 
+// Handle implements the method from TChanServer
 func (f *TChanAuthorizerFilter) Handle(ctx thrift.Context, methodName string, protocol athrift.TProtocol) (success bool, resp athrift.TStruct, err error) {
 	// Here we do a very high level validation to verify user has access on the operation.
 	// We use an empty resource because there is no generic way to get resource from each request.
 	// Each service could do a business logic specific authorization to further verify the user.
+	// TODO we may create a map to change methodName to operation name so the operation name is more user friendly.
 	authorized, err := f.Authorizer.Authorize(ctx, Operation(methodName), EmptyResource)
 	if err != nil {
 		return false, nil, err
 	}
 
 	if !authorized {
-		return false, nil, UnauthorizedError
+		return false, nil, ErrorUnauthorized
 	}
 
 	return f.ChanServer.Handle(ctx, methodName, protocol)
 }
 
+// Service implements the method from TChanServer
 func (f *TChanAuthorizerFilter) Service() string {
 	return f.ChanServer.Service()
 }
 
+// Methods implements the method from TChanServer
 func (f *TChanAuthorizerFilter) Methods() []string {
 	return f.ChanServer.Methods()
 }

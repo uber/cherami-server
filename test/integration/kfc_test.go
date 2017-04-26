dec5a1c7e994ea6cc8257afcd7ec43c113ec8c61
kiranrg <kiranrg@uber.com>
2017-04-26 16:50:43 -0700

    parallelize

--- test/integration/kfc_test.go
+++ test/integration/kfc_test.go
@@ -21,15 +21,14 @@
 package integration
 
 import (
-	"github.com/Shopify/sarama"
-	"github.com/pborman/uuid"
-	"github.com/stretchr/testify/suite"
+	"fmt"
 	"math/rand"
 	"net"
 	"strconv"
-	"testing"
 	"time"
 
+	"github.com/Shopify/sarama"
+	"github.com/pborman/uuid"
 	cheramiclient "github.com/uber/cherami-client-go/client/cherami"
 	"github.com/uber/cherami-server/common"
 	"github.com/uber/cherami-thrift/.generated/go/cherami"
@@ -39,105 +38,104 @@ import (
 // If running on Mac and java 1.7 for ZooKeeper/Kafka, run following command before starting Kafka:
 // echo "127.0.0.1 $HOSTNAME" | sudo tee -a /etc/hosts
 
-type KfCIntegrationSuite struct {
-	testBase
+const (
+	numMsgs          = 1000      // number of messages to send/receive
+	minSize, maxSize = 512, 8192 // range for message size
 
-	destPath, cgPath string
-	cheramiConsumer  cheramiclient.Consumer
-	cheramiMsgsCh    chan cheramiclient.Delivery
-	kafkaProducer    sarama.SyncProducer
-	kafkaTopics      []string
+	nKafkaTopics = 5 // number of kafka topics
+	kafkaBroker  = "localhost:9092"
+	kafkaCluster = "local"
+)
+
+type kafkaMsg struct {
+	topic string
+	key   string
+	val   []byte
+	part  int32
+	offs  int64
+	seq   int
 }
 
-func TestKfCIntegrationSuite(t *testing.T) {
-	s := new(KfCIntegrationSuite)
-	s.testBase.SetupSuite(t)
-	suite.Run(t, s)
+func (t *kafkaMsg) String() string {
+	return fmt.Sprintf("[%d] (topic:%v key:%v val:%d bytes) => (part:%d, offs:%d)", t.seq, t.topic, t.key, len(t.val), t.part, t.offs)
 }
 
-func (s *KfCIntegrationSuite) TestKafkaForCherami() {
+func (s *NetIntegrationSuiteParallelE) TestKafkaForCherami() {
 
-	s.destPath, s.cgPath = "/kafka_test_dest/kfc", "/kafka_test_cg/kfc"
-	s.kafkaTopics = []string{uuid.New(), uuid.New(), uuid.New()}
+	destPath, cgPath := "/kafka_test_dest/kfc", "/kafka_test_cg/kfc"
 
-	// cherami client
+	// initialize set of test kafka topics
+	kafkaTopics := make([]string, nKafkaTopics)
+	for i := range kafkaTopics {
+		kafkaTopics[i] = uuid.New()
+	}
+
+	// create cherami client
 	ipaddr, port, _ := net.SplitHostPort(s.GetFrontend().GetTChannel().PeerInfo().HostPort)
 	portNum, _ := strconv.Atoi(port)
 	cheramiClient := createCheramiClient("cherami-test-kfc-integration", ipaddr, portNum, nil)
 
 	// create cherami kfc destination
 	destDesc, err := cheramiClient.CreateDestination(&cherami.CreateDestinationRequest{
-		Path: common.StringPtr(s.destPath),
+		Path: common.StringPtr(destPath),
 		Type: cherami.DestinationTypePtr(cherami.DestinationType_KAFKA),
 		ConsumedMessagesRetention:   common.Int32Ptr(60),
 		UnconsumedMessagesRetention: common.Int32Ptr(120),
 		ChecksumOption:              cherami.ChecksumOption_CRC32IEEE,
 		OwnerEmail:                  common.StringPtr("cherami-test-kfc-integration@uber.com"),
 		IsMultiZone:                 common.BoolPtr(false),
-		KafkaCluster:                common.StringPtr("local"),
-		KafkaTopics:                 s.kafkaTopics,
+		KafkaCluster:                common.StringPtr(kafkaCluster),
+		KafkaTopics:                 kafkaTopics,
 	})
 	s.NotNil(destDesc)
 	s.NoError(err)
 
 	// create cherami kfc consumer group
 	cgDesc, err := cheramiClient.CreateConsumerGroup(&cherami.CreateConsumerGroupRequest{
-		ConsumerGroupName:    common.StringPtr(s.cgPath),
-		DestinationPath:      common.StringPtr(s.destPath),
+		ConsumerGroupName:    common.StringPtr(cgPath),
+		DestinationPath:      common.StringPtr(destPath),
 		LockTimeoutInSeconds: common.Int32Ptr(30),
 		MaxDeliveryCount:     common.Int32Ptr(1),
 		OwnerEmail:           common.StringPtr("cherami-test-kfc-integration@uber.com"),
 	})
-
 	s.NoError(err)
 	s.NotNil(cgDesc)
 
-	s.cheramiConsumer = cheramiClient.CreateConsumer(&cheramiclient.CreateConsumerRequest{
-		Path:              s.destPath,
-		ConsumerGroupName: s.cgPath,
+	// setup cherami consumer
+	cheramiConsumer := cheramiClient.CreateConsumer(&cheramiclient.CreateConsumerRequest{
+		Path:              destPath,
+		ConsumerGroupName: cgPath,
 		ConsumerName:      "KfCIntegration",
 		PrefetchCount:     1,
 		Options:           &cheramiclient.ClientOptions{Timeout: time.Second * 30}, // this is the thrift context timeout
 	})
-	s.NotNil(s.cheramiConsumer)
-	defer s.cheramiConsumer.Close()
+	s.NotNil(cheramiConsumer)
+	defer cheramiConsumer.Close()
+
+	cheramiMsgsCh, err := cheramiConsumer.Open(make(chan cheramiclient.Delivery, 1))
+	s.NoError(err)
 
 	// setup kafka producer
 	config := sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Return.Successes = true
 
-	s.kafkaProducer, err = sarama.NewSyncProducer([]string{"localhost:9092"}, config)
+	kafkaProducer, err := sarama.NewSyncProducer([]string{kafkaBroker}, config)
 	s.NoError(err)
-	defer s.kafkaProducer.Close()
-
-	s.cheramiMsgsCh, err = s.cheramiConsumer.Open(make(chan cheramiclient.Delivery, 1))
-	s.NoError(err)
-
-	const numMsgs = 10
-
-	type kafkaMsg struct {
-		topic string
-		key   string
-		val   []byte
-		part  int32
-		offs  int64
-	}
+	defer kafkaProducer.Close()
 
 	msgs := make(map[string]*kafkaMsg)
 
 	// publish messages to kafka
 	for i := 0; i < numMsgs; i++ {
 
-		const minSize, maxSize = 512, 8192
-
-		var topic = s.kafkaTopics[rand.Intn(len(s.kafkaTopics))]   // pick one of the topics at random
+		var topic = kafkaTopics[rand.Intn(len(kafkaTopics))]       // pick one of the topics at random
 		var key = uuid.New()                                       // random key
 		var val = make([]byte, minSize+rand.Intn(maxSize-minSize)) // random buf
 		rand.Read(val)                                             // fill 'val' with random bytes
 
 		part, offs, err :=
-			s.kafkaProducer.SendMessage(
+			kafkaProducer.SendMessage(
 				&sarama.ProducerMessage{
 					Topic: topic,
 					Key:   sarama.StringEncoder(key),
@@ -147,7 +145,7 @@ func (s *KfCIntegrationSuite) TestKafkaForCherami() {
 
 		s.NoError(err)
 
-		msgs[key] = &kafkaMsg{topic: topic, key: key, val: val, part: part, offs: offs}
+		msgs[key] = &kafkaMsg{topic: topic, key: key, val: val, part: part, offs: offs, seq: i}
 	}
 
 	// consume messages from cherami
@@ -155,18 +153,22 @@ loop:
 	for i := 0; i < numMsgs; i++ {
 
 		select {
-		case cmsg := <-s.cheramiMsgsCh:
+		case cmsg := <-cheramiMsgsCh:
 			payload := cmsg.GetMessage().Payload
 			uc := payload.GetUserContext()
 
 			msg := msgs[uc["key"]]
+			// fmt.Printf("msg=%v\n", msg)
 
+			// validate msg values are as expected
 			s.Equal(msg.topic, uc["topic"])
 			s.Equal(msg.key, uc["key"])
 			s.Equal(len(msg.val), len(payload.GetData()))
 			s.EqualValues(msg.val, payload.GetData())
 			s.Equal(strconv.Itoa(int(msg.part)), uc["partition"])
 			s.Equal(strconv.Itoa(int(msg.offs)), uc["offset"])
+
+			delete(msgs, msg.key) // ensure we don't see duplicates
 
 			cmsg.Ack()
 

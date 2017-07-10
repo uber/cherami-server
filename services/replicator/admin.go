c73752cc1b413f2f5ac281f5d8aef5c817723db1
Wei Han <weihan@uber.com>
2017-07-10 10:22:22 -0700

    add store connections to dump API

--- services/replicator/admin.go
+++ services/replicator/admin.go
@@ -9,10 +9,20 @@ import (
 // DumpConnectionStatus implements the admin API
 func (r *Replicator) DumpConnectionStatus(ctx thrift.Context) (*admin.ReplicatorConnectionStatus, error) {
 	status := admin.NewReplicatorConnectionStatus()
+
 	r.remoteReplicatorConnMutex.Lock()
 	defer r.remoteReplicatorConnMutex.Unlock()
 	for extent, conn := range r.remoteReplicatorConn {
 		status.RemoteReplicatorConn = append(status.RemoteReplicatorConn, &admin.ReplicatorConnection{
+			ExtentUUID: common.StringPtr(extent),
+			StartTime:  common.Int64Ptr(conn.startTime),
+		})
+	}
+
+	r.storehostConnMutex.Lock()
+	defer r.storehostConnMutex.Unlock()
+	for extent, conn := range r.storehostConn{
+		status.StorehostConn = append(status.StorehostConn, &admin.ReplicatorConnection{
 			ExtentUUID: common.StringPtr(extent),
 			StartTime:  common.Int64Ptr(conn.startTime),
 		})
